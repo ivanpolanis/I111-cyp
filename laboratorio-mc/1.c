a578ab8be23963939e20e125ce64e4475767c713
Ivan Polanis <polanisivan@gmail.com>
2025-01-06 16:30:56 -0300

    openmp done

--- laboratorio-mc/1.c
+++ laboratorio-mc/1.c
@@ -1,27 +1,30 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <threads.h>
 #include <unistd.h>
 
-void initialize_arr(int *arr, int len) {
-  srand(16);
-  for (int i = 0; i < len; i++) {
-    arr[i] = rand() % 29 + 1;
-  }
-}
+int main(int argc, char **argv) {
 
-int main(int argc, char *argv[]) {
-  int n = atoi(argv[1]);
+  if (argc < 3)
+    return -1;
+
+  int N = atoi(argv[1]);
   int threads = atoi(argv[2]);
-  int array[n];
-  initialize_arr(array, n);
-
   omp_set_num_threads(threads);
-#pragma omp parallel for schedule(guided)
-  for (int i = 0; i < n; i++) {
-    sleep(array[i] / 10);
+
+  int vec[N];
+
+#pragma omp parallel for
+  for (int i = 0; i < N; i++) {
+    vec[i] = rand() % 30 + 1;
   }
 
-  printf("Nashe");
+#pragma omp parallel for
+  for (int i = 0; i < N; i++) {
+    printf("%d\n", vec[i]);
+    usleep(vec[i]);
+  }
+
+  return 0;
 }
--- laboratorio-mc/2.c
+++ laboratorio-mc/2.c
@@ -1,52 +1,43 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
-void init_matrix(int **matrix, int n) {
-  int i, j;
-#pragma omp parallel default(private) shared(matrix)
-  {
-    srand((int)(time(NULL)) ^ omp_get_thread_num());
-#pragma omp for schedule(static)
-    for (i = 0; i < n; i++) {
-      for (j = 0; j < n; j++) {
-        printf("thread: %d, i: %d", omp_get_thread_num(), i);
-        matrix[i][j] = rand() % 31 + 1;
-      }
-    }
-  }
-}
-
-void allocate_memory(int **matrix, int n) {
-  for (int i = 0; i < n; i++) {
-    matrix[i] = (int *)malloc(n * sizeof(int));
+void print_matrix(int size, int (*vec)[size]) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++)
+      printf(" %d", vec[i][j]);
+    printf("\n");
   }
 }
 
 int main(int argc, char **argv) {
-  int n = atoi(argv[1]);
+  if (argc < 3)
+    return -1;
+
+  int N = atoi(argv[1]);
   int threads = atoi(argv[2]);
-
-  int *res[n];
-  int *a[n], *b[n];
-
   omp_set_num_threads(threads);
 
-  allocate_memory(a, n);
-  allocate_memory(b, n);
-  allocate_memory(res, n);
+  int vec[N][N];
 
-  init_matrix(a, n);
-  init_matrix(b, n);
+#pragma omp parallel for
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++)
+      vec[i][j] = rand() % 9 + 1;
+  }
 
-#pragma omp parallel for shared(res) schedule(dynamic)
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      for (int k = 0; k < n; k++) {
-        res[i][j] = a[i][k] * b[k][j];
+  int res[N][N];
+// res = vec X vec
+#pragma omp parallel for collapse(2)
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int partial_sum = 0;
+      for (int k = 0; k < N; k++) {
+        partial_sum += vec[i][k] * vec[k][j];
       }
+      res[i][j] = partial_sum;
     }
   }
+
+  return 0;
 }
